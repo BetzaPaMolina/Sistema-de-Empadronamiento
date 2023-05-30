50fa6be90577267ed9c7351db0ae284a89f12e50
Betsabé Pa Molina <lpam@miumg.edu.gt>
2023-05-30 10:06:30 -0600

    :memo: Nuevos Cambios

---
+++ Ciudadanos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SistemaDeEmpadronamiento
+{
+    internal class Ciudadanos
+    {
+        //Instanciamos a la clase conexión
+        private ConexionBD Conexion = new ConexionBD(); 
+        //Instanciamos al objeto sql command para ejecutar instrucciones
+        private SqlCommand Comando = new SqlCommand();
+        //Instanciamos Sql Data Reader para leer filas
+        private SqlDataReader LeerFilas;
+
+        //Creamos un metodo publico de tipo datatable para listar la tabla 
+        public DataTable Listar
+
+    }
+}
